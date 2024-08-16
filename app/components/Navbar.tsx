90754f42ac84a9dde4285137fb56a5b072aaa0fc
aman1357 <amancorp04@gmail.com>
2024-08-16 14:50:18 +0530

    navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='border-white  border '>
-       <h2>SCORExFEED</h2>
-         <p>
-           
-         </p>
+    <div className=' bg-black  w-full h-full flex justify-center  items-center'>
+        
+        <div className='flex flex-col'>
+            <div className='flex justify-center'>
+              <p className='text-green-800 font-semibold'>Scores</p>
+              <p className='text-white font-semibold'>Source</p>
+            </div>
+            <div>
+             <p className='text-xs'>
+              The best Source of Scores and News
+             </p>
+            </div>
+        </div>
     </div>
   )
 }
