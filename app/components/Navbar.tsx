e571849a4a9486dc24e25627358dace76f2a5eff
aman1357 <amancorp04@gmail.com>
2024-09-15 21:28:56 +0530

    navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='border-white  border '>
+       <h2>SCORExFEED</h2>
+         <p>
+           
+         </p>
+    </div>
+  )
+}
+
+export default Navbar
